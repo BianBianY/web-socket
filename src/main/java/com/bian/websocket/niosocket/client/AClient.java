9b26a469ef0242222868552a755713a7b1764952
BianbianY <1712150225@qq.com>
2024-05-23 20:54:03 +0800

    refactor nio-socket

--- src/main/java/com/bian/websocket/niosocket/client/AClient.java
+++ src/main/java/com/bian/websocket/niosocket/client/AClient.java
@@ -1,14 +1,25 @@
 package com.bian.websocket.niosocket.client;
 
 import java.io.IOException;
+import java.io.OutputStream;
+import java.net.Socket;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.TimeUnit;
 
 public class AClient {
 
-    public static void main(String[] args) {
-        try {
-            new ChatClient().startClient("lucy");
-        } catch (IOException e) {
-            e.printStackTrace();
+    public static void main(String[] args) throws IOException, InterruptedException {
+        List<Socket> sockets = new ArrayList<>();
+        for (int i = 0; i < 1000; i++) {
+            TimeUnit.MICROSECONDS.sleep(500);
+            Socket socket = new Socket("127.0.0.1", 9999);
+            OutputStream outputStream = socket.getOutputStream();
+            outputStream.write("你好呀".getBytes());
+            sockets.add(socket);
+        }
+        while (true) {
+            TimeUnit.MICROSECONDS.sleep(500);
         }
     }
 }
